a9aecac85432e22f51ce82896111ef2ac268d97c
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-11-06 09:16:17 +0100

    feat (exNTP): Do the exercice A and B
    the ex A was just to copy and understand the code and the b exercice was to change format of date

--- personnel/ntp/Program.cs
+++ personnel/ntp/Program.cs
@@ -2,3 +2,49 @@
 using System.Net;
 using System.Net.Sockets;
 
+
+Main();
+void Main()
+{
+    string ntpServer = "0.ch.pool.ntp.org";
+
+    byte[] ntpData = new byte[48];
+    ntpData[0] = 0x1b;
+
+    IPEndPoint ntpEndpoint = new IPEndPoint(Dns.GetHostAddresses(ntpServer)[0], 123);
+
+    UdpClient ntpClient = new UdpClient();
+    ntpClient.Connect(ntpEndpoint);
+
+    ntpClient.Send(ntpData, ntpData.Length);
+
+    ntpData = ntpClient.Receive(ref ntpEndpoint);
+
+    NtpPacket ntpPacket = new NtpPacket();
+
+    DateTime ntpTime = ntpPacket.ToDateTime(ntpData);
+
+    Console.WriteLine("Heure actuelle : ");
+    Console.WriteLine($"- {ntpTime.ToString("dddd, dd MMMM yyyy")}");
+    Console.WriteLine($"- {ntpTime.ToString("dd.mm.yyyy HH:mm:ss")}");
+    Console.WriteLine($"- {ntpTime.ToString("dd.mm.yyyy")}\n");
+
+    
+
+    ntpClient.Close();
+
+    Console.ReadLine();
+}
+
+public class NtpPacket()
+{
+    public DateTime ToDateTime(byte[] ntpData)
+    {
+        ulong intPart = (ulong)ntpData[40] << 24 | (ulong)ntpData[41] << 16 | (ulong)ntpData[42] << 8 | (ulong)ntpData[43];
+        ulong fractPart = (ulong)ntpData[44] << 24 | (ulong)ntpData[45] << 16 | (ulong)ntpData[46] << 8 | (ulong)ntpData[47];
+
+        var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+        var networkDateTime = (new DateTime(1900, 1, 1)).AddMilliseconds((long)milliseconds);
+        return networkDateTime;
+    }
+}
