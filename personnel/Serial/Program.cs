771dd3f27c2eeb5a442f9a7f188cf9ba9c7bafca
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-11-06 10:42:18 +0100

    feat (actors): Create the project for the actor's exercice

---
+++ personnel/Serial/Program.cs
@@ -0,0 +1,15 @@
+﻿using Serial;
+using System.Text.Json;
+
+Character Johnny = new Character
+{
+    FirstName = "Peter",
+    LastName = "Parker",
+    Description = "Spider-man",
+    PlayedBy = null
+};
+
+Console.WriteLine(JsonSerializer.Serialize(Johnny));
+
+string serializedCaracter = Johnny.Serialize();
+Johnny.ToJson(serializedCaracter);
